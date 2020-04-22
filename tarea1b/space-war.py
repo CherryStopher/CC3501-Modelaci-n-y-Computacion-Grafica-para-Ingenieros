c44bd1a6449324aee0624a8548f37bb0af1185af
CherryStopher <cristobals80@hotmail.com>
2020-04-22 01:49:01 -0400

    Agregar archivos escenciales para la tarea

---
+++ tarea1b/space-war.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 01:40:08 2020
+
+@author: crist
+"""
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy as np
+import sys
+
+import transformations as tr
+import basic_shapes as bs
+import easy_shaders as es
+
