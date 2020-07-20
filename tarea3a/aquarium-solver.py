7b87f038e933e78f35021f47791b38e11cc8f09c
CherryStopher <cristobals80@hotmail.com>
2020-07-19 20:36:55 -0400

    primera parte listo

--- tarea3a/aquarium-solver.py
+++ tarea3a/aquarium-solver.py
@@ -3,21 +3,33 @@ import numpy as np
 import matplotlib.pyplot as mpl
 from scipy.sparse import csc_matrix
 from scipy.sparse.linalg import spsolve
+import json
+import sys
+
+def jsonDict(filename):
+    with open(filename) as file:
+        data = json.load(file)
+        return data
+    
+archivo = sys.argv[1]
+
+dim = jsonDict(archivo)
 
 # Problem setup
-H = 4
-W = 3
-L = 6
-h = 0.5
-wL = 0.01
-heater_a = 5
-heater_b = 30
-ambient_temperature = 25
+H = int(dim["height"])
+W = int(dim["width"])
+L = int(dim["lenght" ])
+h = 0.3
+wL = float(dim["window_loss"])
+heater_a = int(dim["heater_a"])
+heater_b = int(dim["heater_b"])
+ambient_temperature = int(dim["ambient_temperature"])
+filename = dim["filename"]
 
 # Number of unknowns
-nv = int(W / h) - 1
-nl = int(L / h) - 1
-nh = int(H / h) - 1
+nv = int(W / h) + 1
+nl = int(L / h) + 1
+nh = int(H / h) 
 
 N = nh * nv * nl
 
@@ -30,9 +42,9 @@ def getP(i,j,k):
     return k*nl*nv + j*nv + i
 
 def getIJK(p):
-    i = p % nv
-    k = (p // nv) // nl
-    j = (p - k*nl*nv - i)/nv 
+    i = int(p % nv)
+    k = int((p // nv) // nl)
+    j = int((p - k*nl*nv - i)/nv) 
     
     return (i, j, k)
 
@@ -73,6 +85,26 @@ for i in range(0, nv):
                 A[p, p] = -6
                 b[p] = 0
             
+            # Calentador A
+            elif  i >= nv//3 and i <= 2*nv//3 and j >= 3*nl//5 and j <= 4*nl//5 and k == 0:
+                A[p, p_up] = 0
+                A[p, p_left] = 0
+                A[p, p_right] = 0
+                A[p, p_near] = 0
+                A[p, p_far] = 0
+                A[p, p] = 1
+                b[p] = heater_a
+                
+            # Calentador B
+            elif  i >= nv//3 and i <= 2*nv//3 and j >= nl//5 and j <= 2*nl//5 and k == 0:
+                A[p, p_up] = 0
+                A[p, p_left] = 0
+                A[p, p_right] = 0
+                A[p, p_near] = 0
+                A[p, p_far] = 0
+                A[p, p] = 1
+                b[p] = heater_b
+            
             # left side
             elif i == 0 and 1 <= j and j <= nl - 2 and 1 <= k and k <= nh - 2:
                 A[p, p_up] = 1
@@ -81,7 +113,7 @@ for i in range(0, nv):
                 A[p, p_near] = 1
                 A[p, p_far] = 1
                 A[p, p] = -6
-                b[p] = 2 * h * wL
+                b[p] = -2 * h * wL
                 
             # right side
             elif i == nv - 1 and 1 <= j and j <= nl - 2 and 1 <= k and k <= nh - 2:
@@ -101,7 +133,7 @@ for i in range(0, nv):
                 A[p, p_right] = 1
                 A[p, p_far] = 2
                 A[p, p] = -6
-                b[p] = 2 * h * wL
+                b[p] = -2 * h * wL
                 
                 
             # far side
@@ -143,7 +175,7 @@ for i in range(0, nv):
                 A[p, p_right] = 2
                 A[p, p_far] = 2
                 A[p, p] = -6
-                b[p] = 2 * h * wL + 2 * h * wL
+                b[p] = -2 * h * wL - 2 * h * wL
                 
             # corner lower left far
             elif (i, j, k) == (0, nl-1, 0):
@@ -151,7 +183,7 @@ for i in range(0, nv):
                 A[p, p_right] = 2
                 A[p, p_near] = 2
                 A[p, p] = -6
-                b[p] = -2 * h * wL + 2 * h * wL
+                b[p] = -2 * h * wL - 2 * h * wL
                 
             # corner top left near
             elif (i, j, k) == (0, 0, nh-1):
@@ -159,7 +191,7 @@ for i in range(0, nv):
                 A[p, p_right] = 2
                 A[p, p_near] = 2
                 A[p, p] = -6
-                b[p] = -ambient_temperature + 2 * h * wL + 2 * h * wL
+                b[p] = -ambient_temperature - 2 * h * wL - 2 * h * wL
                 
             # corner top left far
             elif (i, j, k) == (0, nl-1, nh-1):
@@ -167,7 +199,7 @@ for i in range(0, nv):
                 A[p, p_right] = 2
                 A[p, p_near] = 2
                 A[p, p] = -6
-                b[p] = -ambient_temperature + 2 * h * wL - 2 * h * wL
+                b[p] = -ambient_temperature - 2 * h * wL - 2 * h * wL
                 
             # corner lower right near
             elif (i, j, k) == (nv-1, 0, 0):
@@ -175,7 +207,7 @@ for i in range(0, nv):
                 A[p, p_left] = 2
                 A[p, p_far] = 2
                 A[p, p] = -6
-                b[p] = 2 * h * wL - 2 * h * wL
+                b[p] = - 2 * h * wL - 2 * h * wL
                 
             # corner lower right far
             elif (i, j, k) == (nv-1, nl-1, 0):
@@ -183,7 +215,7 @@ for i in range(0, nv):
                 A[p, p_left] = 2
                 A[p, p_near] = 2
                 A[p, p] = -6
-                b[p] = -2 * h * wL - 2 * h * wL
+                b[p] = - 2 * h * wL - 2 * h * wL
                 
             # corner top right near
             elif (i, j, k) == (nv-1, 0, nh-1):
@@ -191,7 +223,7 @@ for i in range(0, nv):
                 A[p, p_left] = 2
                 A[p, p_near] = 2
                 A[p, p] = -6
-                b[p] = -ambient_temperature + 2 * h * wL - 2 * h * wL
+                b[p] = -ambient_temperature - 2 * h * wL - 2 * h * wL
                 
             # corner top right far
             elif (i, j, k) == (nv-1, nl-1, nh-1):
@@ -208,7 +240,7 @@ for i in range(0, nv):
                 A[p, p_near] = 1
                 A[p, p_far] = 1
                 A[p, p] = -6
-                b[p] = -ambient_temperature + 2 * h * wL
+                b[p] = -ambient_temperature - 2 * h * wL
                 
             # corner bot left
             elif i == 0  and 1 <= j and j <= nl - 2 and k == 0:
@@ -217,7 +249,7 @@ for i in range(0, nv):
                 A[p, p_near] = 1
                 A[p, p_far] = 1
                 A[p, p] = -6
-                b[p] = 2 * h * wL
+                b[p] = - 2 * h * wL
                 
             # corner bot right
             elif i == nv - 1  and 1 <= j and j <= nl - 2 and k == 0:
@@ -226,7 +258,7 @@ for i in range(0, nv):
                 A[p, p_near] = 1
                 A[p, p_far] = 1
                 A[p, p] = -6
-                b[p] = -2 * h * wL
+                b[p] = - 2 * h * wL
                 
             # corner top right
             elif i == nv - 1  and 1 <= j and j <= nl - 2 and k == nh - 1:
@@ -235,16 +267,79 @@ for i in range(0, nv):
                 A[p, p_near] = 1
                 A[p, p_far] = 1
                 A[p, p] = -6
-                b[p] = -ambient_temperatur -2 * h * wL
+                b[p] = -ambient_temperature -2 * h * wL
                 
             # corner top near
-        elif 1 <= i and i <= nv - 2  and j == 0 and k = nh - 1:
+            elif 1 <= i and i <= nv - 2  and j == 0 and k == nh - 1:
                 A[p, p_down] = 1
+                A[p, p_right] = 1
                 A[p, p_left] = 1
                 A[p, p_far] = 2
                 A[p, p] = -6
-                b[p] = -ambient_temperatur -2 * h * wL
+                b[p] = -ambient_temperature - 2 * h * wL
             
+            # corner top far
+            elif 1 <= i and i <= nv - 2  and j == nl - 1 and k == nh - 1:
+                A[p, p_down] = 1
+                A[p, p_right] = 1
+                A[p, p_left] = 1
+                A[p, p_near] = 2
+                A[p, p] = -6
+                b[p] = -ambient_temperature - 2 * h * wL
+            
+            # corner bot near
+            elif 1 <= i and i <= nv - 2  and j == 0 and k == 0:
+                A[p, p_up] = 2
+                A[p, p_right] = 1
+                A[p, p_left] = 1
+                A[p, p_far] = 2
+                A[p, p] = -6
+                b[p] = - 2 * h * wL
+            
+            # corner bot far
+            elif 1 <= i and i <= nv - 2  and j == nl - 1 and k == 0:
+                A[p, p_up] = 2
+                A[p, p_right] = 1
+                A[p, p_left] = 1
+                A[p, p_near] = 2
+                A[p, p] = -6
+                b[p] = - 2 * h * wL
+                
+            # corner left near
+            elif i == 0 and j == 0 and 1 <= k and k <= nh - 2:
+                A[p, p_up] = 1
+                A[p, p_right] = 2
+                A[p, p_down] = 1
+                A[p, p_far] = 2
+                A[p, p] = -6
+                b[p] = - 2 * h * wL - 2 * h * wL
+                
+            # corner left far
+            elif i == 0 and j == nl - 1 and 1 <= k and k <= nh - 2:
+                A[p, p_up] = 1
+                A[p, p_right] = 2
+                A[p, p_down] = 1
+                A[p, p_near] = 2
+                A[p, p] = -6
+                b[p] = - 2 * h * wL - 2 * h * wL
+                
+            # corner right near
+            elif i == nv - 1 and j == 0 and 1 <= k and k <= nh - 2:
+                A[p, p_up] = 1
+                A[p, p_left] = 2
+                A[p, p_down] = 1
+                A[p, p_far] = 2
+                A[p, p] = -6
+                b[p] = - 2 * h * wL - 2 * h * wL
+                
+            # corner right far
+            elif i == nv - 1 and j == nl - 1 and 1 <= k and k <= nh - 2:
+                A[p, p_up] = 1
+                A[p, p_left] = 2
+                A[p, p_down] = 1
+                A[p, p_far] = 2
+                A[p, p] = -6
+                b[p] = - 2 * h * wL - 2 * h * wL
                 
             else:
                 print("Point (" + str(i) + ", " + str(j) + ") missed!")
@@ -253,41 +348,53 @@ for i in range(0, nv):
             
             
         
-        
-        
+      
 # Solving our system
 x = spsolve(A, b)
 
-ux = []
-uy = []
-uz = []
-uc = []
+     
+u = np.zeros((nv,nl,nh))
 
-for p in range(0,N):
+for p in range(0, N):
     i,j,k = getIJK(p)
-    uc += [int(x[p])]
-    ux += [i]
-    uy += [j]
-    uz += [k]
+    u[i, j, k] = x[p]        
+
+# Adding the borders, as they have known values
+ub = np.zeros((nv, nl, nh+1))
+ub[0:nv, 0:nl, 0:nh] = u[:,:,:]
+# Dirichlet boundary condition
+# top 
+ub[0:nv, 0:nl, nh] = ambient_temperature
+
+# Guardando la matriz
+np.save(filename, ub)
+
+
+# Plot
+X, Y, Z = np.mgrid[0:W:11j, 0:L:21j, 0:H:14j]
 
 from mpl_toolkits import mplot3d 
 import matplotlib.pyplot as plt
-
+        
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.scatter(ux, uy, uz, c=uc,
-           cmap='viridis', linewidth=0.5)
+
+surf=ax.scatter(X, Y, Z, c=ub, cmap='winter',
+            linewidth=0.5, marker="o", alpha=1, edgecolor="k")
+
+fig.colorbar(surf, shrink=0.8, aspect=6)
+
+ax.set_title('Preview')
+ax.set_xlabel("Width")
+ax.set_ylabel("Length")
+ax.set_zlabel("Hight")
+
 plt.show()
+   
         
         
-        
-        
-        
-        
-        
-        
-        
-        
+   
+   
         
         
         
---
+++ tarea3a/aquarium-view.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import transformations as tr
+import basic_shapes as bs
+import easy_shaders as es
+import lighting_shaders as ls
+import scene_graph as sg
